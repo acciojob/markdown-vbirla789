9059f877ff054dd31ca40193d6f3295486efea82
VIIBII <birlavishal90@gmail.com>
2023-09-15 01:20:32 +0530

    submitting code-Fri Sep 15 2023 01:20:30 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,76 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import "../styles/App.css";
 
+function App() {
+  const [markdown, setMarkdown] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  const changeLoadingVar = () => {
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    setTimeout(changeLoadingVar, 3000);
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="app">
+      <textarea onChange={handleChange} />
+      <ReactMarkdown className="preview" children={markdown} />
+    </div>
+  );
+}
+
+// class App extends React.Component{
+
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       markdown: "",
+//       loading: true
+//     }
+//     this.handleChange = this.handleChange.bind(this);
+//     this.changeLoadingVar = this.changeLoadingVar.bind(this);
+//   }
+//   handleChange(e){
+//     this.setState({
+//       markdown: e.target.value
+//     })
+//   }
+//   changeLoadingVar(){
+//     this.setState({
+//       loading: false
+//     })
+//   }
+
+//   componentDidMount(){
+//     setTimeout(this.changeLoadingVar,3000)
+//   }
+//   render(){
+//     if(this.state.loading){
+//       return (<div><h1>Loading...</h1></div>)
+//     }
+
+//     return(
+//       <div className="app">
+//         <textarea onChange={this.handleChange}/>
+//         <ReactMarkdown className="preview" children={this.state.markdown}/>
+//       </div>
+//     )
+//   }
+// }
+
+export default App;
